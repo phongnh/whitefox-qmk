8641f2863da375b555675211a5c341d26a5543fe
Phong Nguyen <nhphong1406@gmail.com>
2021-09-26 16:52:32 +0700

    Import project

---
+++ rules.mk
@@ -0,0 +1,14 @@
+MOUSEKEY_ENABLE  = yes # Mouse keys(+4700)
+EXTRAKEY_ENABLE  = yes # Audio control and System control(+450)
+CONSOLE_ENABLE   = no  # Console for debug
+COMMAND_ENABLE   = no  # Commands for debug and configuration
+NKRO_ENABLE      = no
+
+# Keyboard-specific features
+BACKLIGHT_ENABLE  = no
+VISUALIZER_ENABLE = no
+
+# Firmware size reduction
+GRAVE_ESC_ENABLE   = yes
+MAGIC_ENABLE       = no
+SPACE_CADET_ENABLE = no
