b85756dbe01c340cfb3b86c302704bac813864c1
Phong Nguyen <nhphong1406@gmail.com>
2021-09-26 18:42:39 +0700

    Tweak config

--- keymap.c
+++ keymap.c
@@ -29,7 +29,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_FUN] = KEYMAP( \
-        KC_SLEP,          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______,   \
+        KC_SLEP,          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, \
         KC_WAKE,          _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, BL_DEC,  BL_INC,  KC_PSCR,          KC_INS,  \
         _______,          AG_NORM, AG_SWAP, AG_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_RCTL,          KC_HOME, \
         _______, _______, BL_TOGG, BL_STEP, BL_BRTG, _______, RESET,   _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,          KC_VOLU,          KC_END,  \
